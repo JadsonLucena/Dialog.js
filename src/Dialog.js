1f74c66be3ed7a4838eb4169465b753d8251cbe9
JadsonLucena <jadsonlucena@gmail.com>
2021-03-24 22:12:41 -0300

    feat (show): insert styles
    If the fullScreen flag is active, the dialog will occupy the entire screen.

--- src/Dialog.js
+++ src/Dialog.js
@@ -67,6 +67,25 @@ class Dialog {
             delegatesFocus: this.#delegatesFocus
         });
 
+
+        let css = document.createElement('style');
+        css.textContent = `
+            :host, :host * { margin: 0; padding: 0; box-sizing: border-box; }
+            :host { outline: none; position: fixed; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.32); backdrop-filter: blur(0.5rem); overflow: hidden; border: none; z-index: 999999999; }
+                :host > aside { position: absolute; padding: 10px; background-color: #fff; color: #000; transition: 0.3s; box-shadow: 0 2px 8px rgba(0, 0, 0, .33); display: flex; flex-direction: column; }
+                ${fullScreen ? `
+                    :host > aside { padding: 0; width: 100%; height: 100%; }
+                ` : `
+                    :host > aside { left: 50%; top: 50%; transform: translate(-50%, -50%); width: max-content; max-width: calc(100% - 20px); height: max-content; max-height: calc(100% - 20px); border-radius: 3px; }
+                `}
+                    :host > aside > header { position: relative; width: 100%; font-size: 18px; text-align: center; display: flex; flex-direction: row; align-items: center; /*border-bottom: 1px solid #eee;*/ z-index: 2; }
+                    :host > aside > header > h1 { display: inline-block; padding: 0 10px; width: 100%; height: max-content; font-size: 20px; text-align: center; overflow: hidden; white-space: nowrap; text-overflow: ellipsis; }
+                    :host > aside > main { flex: 1; position: relative; padding: 10px; width: 100%; overflow: auto; z-index: 1; }
+                    :host > aside > footer { position: relative; padding: 5px; width: 100%; text-align: center; /*border-bottom: 1px solid #eee;*/ z-index: 3; }
+                    :host > aside > footer:empty { padding: 0; }
+        ` + style + this.#style;
+        dialog.shadowRoot.append(css);
+
     }
 
 }
